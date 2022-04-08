449400d53e204cea17d273ae6d9e596c4cd31b8c
youmari <youmari300@gmail.com>
2022-04-08 14:20:28 +0000

    Set up Apollo client with cache in the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache,
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
 );
+
+export default cache;
