0d4551441aa9e6a35ce082ecaa0c4c622406c5d2
youmari <youmari300@gmail.com>
2022-04-08 14:22:06 +0000

    Add nav bar categories links and add more currencies

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = ({ onChangeCurrency }) => (
-  <select className="select" onChange={onChangeCurrency}>
-    <option value="$"> $ USD</option>
-    <option value="£"> £ GBP</option>
-    <option value="¥"> ¥ JPY</option>
-  </select>
+  <>
+    <NavLink
+      to="/"
+      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+    >
+      All
+    </NavLink>
+    <NavLink
+      to="/tech"
+      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+    >
+      tech
+    </NavLink>
+    <NavLink
+      to="/clothes"
+      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+    >
+      clothes
+    </NavLink>
+    <select className="select" onChange={onChangeCurrency}>
+      <option value="$">$ USD</option>
+      <option value="£">£ GBP</option>
+      <option value="¥">¥ JPY</option>
+      <option value="₽">₽ RUB</option>
+      <option value="A$">A$ AUD</option>
+    </select>
+  </>
 );
 
 export default NavBar;
