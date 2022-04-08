c0f8fe1515833c91860ee4421c4eaade68dad0bc
youmari <youmari300@gmail.com>
2022-04-08 14:25:12 +0000

    Fetch data from apollo and display them in a product component

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,44 +1,35 @@
 import React, { Component } from 'react';
 import { Query } from '@apollo/client/react/components';
-import { queryGenerator } from './queries';
+import ProductItem from '../ProductItem/ProductItem';
+import { queryGenerator } from '../../queries/queries';
 
 export class ProductList extends Component {
   constructor(props) {
     super(props);
   }
 
-  data = ({ data, loading, error }) => {
-    const { symbol } = this.props;
-    if (loading) return <h1>Loading....</h1>;
-    const { name, products } = data.category;
-    return (
-      <section>
-        <h1>{name}</h1>
-        {products.map((product) => (
-          <article key={product.id}>
-            <img
-              style={{ width: 200 }}
-              src={product.gallery[0]}
-              alt={product.name}
-            />
-            <h4>{product.name}</h4>
-            {product.prices.map(
-              (price) =>
-                price.currency.symbol === symbol && (
-                  <strong key={price.currency.symbol}>
-                    {price.currency.symbol}
-                    {price.amount}
-                  </strong>
-                ),
-            )}
-          </article>
-        ))}
-      </section>
-    );
-  };
-
   render() {
-    return <Query query={queryGenerator('all')}>{this.data}</Query>;
+    const { symbol, category } = this.props;
+    return (
+      <Query query={queryGenerator(category)}>
+        {({ data, loading }) => {
+          if (loading) return <h1>loading...</h1>;
+          const { name, products } = data.category;
+          return (
+            <section>
+              <h1>{name}</h1>
+              {products.map((product) => (
+                <ProductItem
+                  key={product.id}
+                  symbol={symbol}
+                  product={product}
+                />
+              ))}
+            </section>
+          );
+        }}
+      </Query>
+    );
   }
 }
 export default ProductList;
