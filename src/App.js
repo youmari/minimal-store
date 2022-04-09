e777ccedb55e5814cfccb1dd4ced82a7f9fc72c3
youmari <youmari300@gmail.com>
2022-04-09 14:02:08 +0000

    Clean App.js and install react-router v6

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from './Components/Navbar/NavBar';
+import ProductListPage from './Pages/ProductListingPage.js/ProductListPage';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      symbol: localStorage.getItem('symbol') || '$',
-    };
-  }
-
-  handleOnChange = (event) => {
-    const { value } = event.target;
-    this.setState({ symbol: value });
-    localStorage.setItem('symbol', value);
-  };
-
-  handleCurrencyOnLoad = () => {
-    const elem = document.querySelector('.select');
-    elem.value = localStorage.getItem('symbol');
-  };
-
   render() {
-    window.addEventListener('load', this.handleCurrencyOnLoad);
-    const { symbol } = this.state;
     return (
       <>
-        <NavBar onChangeCurrency={this.handleOnChange} />
+        <ProductListPage />
       </>
     );
   }
