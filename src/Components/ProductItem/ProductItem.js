d1d4a635062f6cead6328e03d4157d1cb60bb270
youmari <youmari300@gmail.com>
2022-04-10 18:45:13 +0000

    Add to cart fuctionality in productlisting page

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { addProductToCart } from '../../Redux/Cart/cart';
 
 class ProductItem extends Component {
@@ -8,32 +9,38 @@ class ProductItem extends Component {
   }
 
   handleAddProductToCart = (id) => {
-    const {addProductToCart} = this.props
-    addProductToCart(id)
+    const { addProductToCart } = this.props;
+    addProductToCart(id);
   };
 
   render() {
     const { product, symbol } = this.props;
     return (
       <article key={product.id}>
-        <img
-          style={{ width: 200 }} // I will remove it later
-          src={product.gallery[0]}
-          alt={product.name}
-        />
-        <button type="button" onClick={() => {this.handleAddProductToCart(product.id)}}>
+        <Link to={product.id}>
+          <img
+            style={{ width: 200 }} // I will remove it later
+            src={product.gallery[0]}
+            alt={product.name}
+          />
+          <h4>{product.name}</h4>
+        </Link>
+        {product.prices.map(
+          (price) => price.currency.symbol === symbol && (
+          <strong key={price.currency.symbol}>
+            {price.currency.symbol}
+            {price.amount}
+          </strong>
+          ),
+        )}
+        <button
+          type="button"
+          onClick={() => {
+            this.handleAddProductToCart(product.id);
+          }}
+        >
           add to cart
         </button>
-        <h4>{product.name}</h4>
-        {product.prices.map(
-          (price) =>
-            price.currency.symbol === symbol && (
-              <strong key={price.currency.symbol}>
-                {price.currency.symbol}
-                {price.amount}
-              </strong>
-            ),
-        )}
       </article>
     );
   }
