09640298c6f0b32226e1b5cfeceff8eb8dbd518c
youmari <youmari300@gmail.com>
2022-04-17 18:06:31 +0000

    Add cart button to the nav bar

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
-const NavBar = ({ onChangeCurrency }) => (
-  <>
-    <NavLink
-      to="/"
-      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
-    >
-      All
-    </NavLink>
-    <NavLink
-      to="/tech"
-      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
-    >
-      tech
-    </NavLink>
-    <NavLink
-      to="/clothes"
-      // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
-    >
-      clothes
-    </NavLink>
-    <select className="select" onChange={onChangeCurrency}>
-      <option value="$">$ USD</option>
-      <option value="£">£ GBP</option>
-      <option value="¥">¥ JPY</option>
-      <option value="₽">₽ RUB</option>
-      <option value="A$">A$ AUD</option>
-    </select>
-  </>
-);
+import cartIcon from '../../Assets/cartIcon.svg';
+import Minicart from '../MiniCart/MiniCart';
+const NavBar = ({ onChangeCurrency, symbol }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+      >
+        All
+      </NavLink>
+      <NavLink
+        to="/tech"
+        // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+      >
+        tech
+      </NavLink>
+      <NavLink
+        to="/clothes"
+        // className={({ isActive }) => (isActive ? activeLinkStyles : linkStyles)}
+      >
+        clothes
+      </NavLink>
+      <select className="select" onChange={onChangeCurrency}>
+        <option value="$">$ USD</option>
+        <option value="£">£ GBP</option>
+        <option value="¥">¥ JPY</option>
+        <option value="₽">₽ RUB</option>
+        <option value="A$">A$ AUD</option>
+      </select>
+      <button
+        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+        className="cart-btn"
+        type="button"
+      >
+        <img src={cartIcon} />
+      </button>
+      {isOpen && <Minicart symbol={symbol} setIsOpen={setIsOpen} />}
+    </>
+  );
+};
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import store from './Redux/store/store';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './Redux/store/store';
 import App from './App';
 import './index.css';
 
