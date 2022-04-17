4bd9caf4a55c3cf14284bdbab38b792aec01ded2
youmari <youmari300@gmail.com>
2022-04-17 17:59:23 +0000

    Add function to get the total price

--- src/Components/MiniCart/MiniCart.js
+++ src/Components/MiniCart/MiniCart.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class Minicart extends Component {
+  totalPrice = () => {
+    const { cart, symbol } = this.props;
+    let total = 0;
+    cart.forEach((item) => {
+      item.prices.forEach((price) => {
+        if (price.currency.symbol === symbol) {
+          total += price.amount * item.quantity;
+        }
+      });
+    });
+    return `${symbol}${total.toFixed(2)}`;
+  };
   render() {
     return (
       <div>
