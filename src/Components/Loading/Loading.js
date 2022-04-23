0234390bcf664588068f4a6e8eeac5f5d85a8910
youmari <youmari300@gmail.com>
2022-04-23 17:54:23 +0000

    Add the loading component with its style

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Loading.style.css';
 
 const Loading = () => (
-  <div>
-    loading....
+  <div className="container">
+    <div className="span-container">
+      <span className="one ball" />
+      <span className="two ball" />
+      <span className="three ball" />
+      <span className="four ball" />
+    </div>
   </div>
 );
 
