9b223c8346ecda44cc1052dda0a612bd2b9caf67
youmari <youmari300@gmail.com>
2022-04-23 17:57:03 +0000

    Add loading

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Query } from '@apollo/client/react/components';
 import ProductItem from '../ProductItem/ProductItem';
 import { queryGenerator } from '../../queries/queries';
+import './ProductList.style.css';
+import Loading from '../Loading/Loading';
 
 const ProductList = ({ symbol, category }) => (
   <Query query={queryGenerator(category)}>
     {({ data, loading }) => {
-      if (loading) return <h1>loading...</h1>;
+      if (loading) return <Loading />;
       const { name, products } = data.category;
       return (
         <section>
-          <h1>{name}</h1>
-          {products.map((product) => (
-            <ProductItem key={product.id} symbol={symbol} product={product} />
-          ))}
+          <h1 className="category-title">{name}</h1>
+          <div className="products-container">
+            {products.map((product) => (
+              <ProductItem key={product.id} symbol={symbol} product={product} />
+            ))}
+          </div>
         </section>
       );
     }}
