530ac11750af57949db70a543b09ad2e6917338e
youmari <youmari300@gmail.com>
2022-05-27 17:53:31 +0100

    Change AddToCart logic

--- src/Redux/Cart/cart.js
+++ src/Redux/Cart/cart.js
@@ -3,6 +3,7 @@ import cache from '../..';
 const initialState = {
   cart: [],
 };
+let num = 0;
 
 const ADD_TO_CART = 'MINIMAL_STORE/CART/ADD_TO_CART';
 const REMOVE_FROM_CART = 'MINIMAL_STORE/CART/REMOVE_FROM_CART';
@@ -21,38 +22,57 @@ export const removeProductFromCart = (id) => ({
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART: {
+      let cartId = null;
+      let mt = 0;
+      const attributeChecker = new Set();
       const productsObject = cache?.data?.data;
       const productData = productsObject[`Product:${action.id}`];
-      const isProductInCart = state.cart.find(
+      const ProductInCart = state.cart.some(
         (item) => item.id === productData.id,
       );
+      if (ProductInCart) {
+        action.attributes.forEach((attribute) => {
+          attributeChecker.add(attribute.value);
+        });
+        for (const cartItem of state.cart) {
+          const { attributes } = cartItem;
+          if (mt === attributeChecker.size) break;
+          for (const attribute of attributes) {
+            if (attributeChecker.has(attribute.value)) {
+              cartId = cartItem.cartId;
+              mt++;
+            } else {
+              mt = 0;
+            }
+          }
+        }
+        if (mt === attributeChecker.size) {
+          return {
+            ...state,
+            cart: [...state.cart.map((product) => {
+              if (cartId !== product.cartId) {
+                return product;
+              }
+              return {
+                ...product, quantity: product.quantity + 1,
+              };
+            })],
+          };
+        }
+      }
       return {
         ...state,
-        cart: !state.cart.length
-          ? [
-            ...state.cart,
-            { ...productData, quantity: 1, attributes: action.attributes },
-          ]
-          : isProductInCart
-            ? [
-              ...state.cart.map((product) => {
-                if (product.id !== productData.id) {
-                  return product;
-                }
-                return {
-                  ...product,
-                  quantity: product.quantity + 1,
-                  attributes: action.attributes,
-                };
-              }),
-            ]
-            : [
-              ...state.cart,
-              { ...productData, quantity: 1, attributes: action.attributes },
-            ],
+        cart: [
+          ...state.cart,
+          {
+            ...productData,
+            quantity: 1,
+            attributes: action.attributes,
+            cartId: num++,
+          },
+        ],
       };
     }
-
     default:
       return state;
   }
